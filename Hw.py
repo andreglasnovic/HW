bd5064e7d4b2182c82183c88f027e255d4074423
andreglasnovic <aglasnovic@icloud.com>
2022-02-02 21:26:22 -0500

    initial commit

--- Hw.py
+++ Hw.py
@@ -1,20 +1,8 @@
 
-import numpy as np
-#Original Matrix
 x = [[1,2],
-    [3,4]]
+    [3,4],
+    [5,6]]
 
+result = [[x[j][i] for j in range(len(x))] for i in range(len(x[0]))]
 
-def transpose_matrix(x):
-    result = [[0, 0], [0, 0]]
-    # Iterate through rows
-    for i in range(len(x)):
-    #Iterate through columns
-        for j in range(len(x[0])):
-            result[j][i] = x[i][j]
-    for r in result:
-        print(r)
-
-transpose_matrix(x)
-
-print(np.transpose(x))
+print(result)
